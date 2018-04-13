170f221314f106ec27f79370d20e9ae0efff831b
s610473 <s610473@004-50527-190S>
2018-04-13 13:29:13 -0500

    change2

--- Ung Bradley Board Game/Form1.Designer.vb
+++ Ung Bradley Board Game/Form1.Designer.vb
@@ -30,7 +30,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(123, 112)
+        Me.Label1.Location = New System.Drawing.Point(98, 28)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 0
