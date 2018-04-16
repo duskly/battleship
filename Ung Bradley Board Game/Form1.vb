77c04bb8278019b5e0b70d3ab2bf85d4a38e3cbc
s610473 <s610473@004-50527-190S>
2018-04-16 13:53:02 -0500

    ehhhh

--- Ung Bradley Board Game/Form1.vb
+++ Ung Bradley Board Game/Form1.vb
@@ -1,13 +1,41 @@
 ﻿Public Class gameForm
-    Dim grid() As Control = {Label1}
-
-    Private Sub box_MouseHover(sender As Object, e As EventArgs)
-        MessageBox.Show("wooo")
-    End Sub
 
     Private Sub gameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For Each lb As Label In grid
-            AddHandler lb.MouseHover, AddressOf box_MouseHover
+        Dim cellBase As Label
+
+        cellBase.BorderStyle = BorderStyle.FixedSingle
+
+
+        Dim grid() As Label = {}
+
+        For k As Integer = 0 To 7
+            For i As Integer = 0 To 7
+                cellBase.Name = "cell" + (k * 8 + i + 1).ToString
+                Me.Controls.Add(cellBase)
+
+                cellBase.Size = New System.Drawing.Size(30, 30)
+                cellBase.Location = New System.Drawing.Point(30, 30)
+                grid(k * 8 + i) = cellBase
+            Next
         Next
+
+
+
+        For Each lb As Label In grid
+            AddHandler lb.MouseEnter, AddressOf cell_MouseEnter
+            AddHandler lb.MouseLeave, AddressOf cell_MouseLeave
+        Next
+    End Sub
+
+    Private Sub cell_MouseEnter(sender As Object, e As EventArgs)
+        Dim cell = DirectCast(sender, Label)
+
+        cell.BackColor = Color.White
+    End Sub
+
+    Private Sub cell_MouseLeave(sender As Object, e As EventArgs)
+        Dim cell = DirectCast(sender, Label)
+
+        cell.BackColor = Me.BackColor
     End Sub
 End Class
