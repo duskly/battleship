4ee8d427eb92bfc2ad75352c7f6fc5e8d9df49fe
s610473 <s610473@004-50527-190S>
2018-04-17 13:45:26 -0500

    progress

--- Ung Bradley Board Game/Form1.Designer.vb
+++ Ung Bradley Board Game/Form1.Designer.vb
@@ -22,16 +22,42 @@ Partial Class gameForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.exitBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(30, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(224, 224)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        Me.Label1.Visible = False
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Location = New System.Drawing.Point(178, 379)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(75, 23)
+        Me.exitBtn.TabIndex = 1
+        Me.exitBtn.Text = "Exit"
+        Me.exitBtn.UseVisualStyleBackColor = True
         '
         'gameForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(615, 491)
+        Me.Controls.Add(Me.exitBtn)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "gameForm"
         Me.Text = "Battleship"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents exitBtn As Button
 End Class
--- Ung Bradley Board Game/Form1.vb
+++ Ung Bradley Board Game/Form1.vb
@@ -1,41 +1,63 @@
 ﻿Public Class gameForm
 
+    'Global variables for the initial ship placing phase
+    Dim placePhase As Boolean = False
+    Dim rotation As Boolean = False
+    Dim ableToPlaceShip As Boolean = False
+    Dim shipLengthToPlace As Integer = 5
+
     Private Sub gameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim cellBase As Label
+        placePhase = True
 
-        cellBase.BorderStyle = BorderStyle.FixedSingle
-
-
-        Dim grid() As Label = {}
+        'Generates grid, assigns each cell properties
+        Dim grid(63) As Label
 
         For k As Integer = 0 To 7
             For i As Integer = 0 To 7
-                cellBase.Name = "cell" + (k * 8 + i + 1).ToString
+                Dim cellIndex As Integer = k * 8 + i
+
+                Dim cellBase As New Label
+                cellBase.Name = "cell" + (cellIndex + 1).ToString
+                cellBase.BorderStyle = BorderStyle.FixedSingle
+                cellBase.Location = New Point(30 + 28 * i, 30 + 28 * k)
+                cellBase.Size = New Size(30, 30)
+                cellBase.Tag = cellIndex
                 Me.Controls.Add(cellBase)
 
-                cellBase.Size = New System.Drawing.Size(30, 30)
-                cellBase.Location = New System.Drawing.Point(30, 30)
-                grid(k * 8 + i) = cellBase
+                grid(cellIndex) = cellBase
             Next
         Next
-
-
 
         For Each lb As Label In grid
             AddHandler lb.MouseEnter, AddressOf cell_MouseEnter
             AddHandler lb.MouseLeave, AddressOf cell_MouseLeave
+            AddHandler lb.MouseClick, AddressOf cell_MouseClick
         Next
     End Sub
 
     Private Sub cell_MouseEnter(sender As Object, e As EventArgs)
         Dim cell = DirectCast(sender, Label)
+        Dim cellNum As Integer
+        Integer.TryParse(cell.Tag, cellNum)
+        Dim row As Integer = (cellNum / 8) - 1
+        Dim column As Integer = cellNum Mod 8
 
-        cell.BackColor = Color.White
+
     End Sub
 
     Private Sub cell_MouseLeave(sender As Object, e As EventArgs)
         Dim cell = DirectCast(sender, Label)
 
         cell.BackColor = Me.BackColor
+    End Sub
+
+    Private Sub cell_MouseClick(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub gameForm_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = " " Then
+            MessageBox.Show("nice")
+        End If
     End Sub
 End Class
