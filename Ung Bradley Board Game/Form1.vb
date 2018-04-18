7000af5f313f15855a0e72e2373a47538d75c43f
s610473 <s610473@004-50527-190S>
2018-04-18 13:45:14 -0500

    nothing

--- Ung Bradley Board Game/Form1.vb
+++ Ung Bradley Board Game/Form1.vb
@@ -1,17 +1,27 @@
 ﻿Public Class gameForm
 
+
+
     'Global variables for the initial ship placing phase
     Dim placePhase As Boolean = False
-    Dim rotation As Boolean = False
+    Dim shipRotated As Boolean = False
     Dim ableToPlaceShip As Boolean = False
     Dim shipLengthToPlace As Integer = 5
+    Dim filledCells() As Integer
+    Dim shipCellIndex() As Integer
+    Dim validPlacement As Boolean = False
+    Dim grid(63) As Label
+
+    Private Sub fillCells(ByVal x As Integer, y As Integer)
+
+
+
+    End Sub
 
     Private Sub gameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         placePhase = True
 
-        'Generates grid, assigns each cell properties
-        Dim grid(63) As Label
-
+        'Generates grid
         For k As Integer = 0 To 7
             For i As Integer = 0 To 7
                 Dim cellIndex As Integer = k * 8 + i
@@ -35,27 +45,51 @@
         Next
     End Sub
 
+    Private Sub cell_MouseLeave(sender As Object, e As EventArgs)
+        Dim cell = DirectCast(sender, Label)
+
+        'cell.BackColor = Me.BackColor
+    End Sub
+
     Private Sub cell_MouseEnter(sender As Object, e As EventArgs)
         Dim cell = DirectCast(sender, Label)
         Dim cellNum As Integer
         Integer.TryParse(cell.Tag, cellNum)
         Dim row As Integer = (cellNum / 8) - 1
         Dim column As Integer = cellNum Mod 8
+        MessageBox.Show(column.ToString + " " + row.ToString)
+        fillCells(column, row)
 
+        If shipRotated Then
 
-    End Sub
+        Else
 
-    Private Sub cell_MouseLeave(sender As Object, e As EventArgs)
-        Dim cell = DirectCast(sender, Label)
+            For k As Integer = column To column + shipLengthToPlace
 
-        cell.BackColor = Me.BackColor
+                Dim cellIndex As Integer = row * 8 + k
+
+                If k > 7 Then
+                    validPlacement = False
+                    Exit For
+                Else
+
+                    If shipCellIndex.Contains(cellIndex) Then
+                        validPlacement = False
+                    Else
+
+                        grid(cellIndex).BackColor = Color.DarkGray
+                    End If
+                End If
+            Next
+        End If
     End Sub
 
     Private Sub cell_MouseClick(sender As Object, e As EventArgs)
+        shipLengthToPlace -= 2
 
     End Sub
 
-    Private Sub gameForm_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+    Private Sub gameForm_SpaceKeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
         If e.KeyChar = " " Then
             MessageBox.Show("nice")
         End If
